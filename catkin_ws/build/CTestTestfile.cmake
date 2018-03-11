a0b7a03a7b7f8ba1619cdfa4f79b0784e2b449b1
zhaiwenpeng <1460760286@qq.com>
2018-03-11 16:04:46 +0800

    added catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhai/catkin_ws/src
+# Build directory: /home/zhai/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(serial)
+subdirs(teleop_twist_keyboard)
+subdirs(base_controller)
+subdirs(beginner_tutorials)
+subdirs(catkin_rplidar/src/rplidar_ros)
---
+++ catkin_ws/build/catkin_rplidar/src/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhai/catkin_ws/src/catkin_rplidar/src/rplidar_ros
+# Build directory: /home/zhai/catkin_ws/build/catkin_rplidar/src/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
